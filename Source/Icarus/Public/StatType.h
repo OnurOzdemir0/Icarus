e2bbae64f068ceb2f2ddef2b11b5228d3ff0332d
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2023-09-21 01:16:44 +0300

    playerstat

---
+++ Source/Icarus/Public/StatType.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "StatType.generated.h"
+
+UENUM(BlueprintType)
+enum class EStatType : uint8
+{
+	EST_Ammo UMETA(DisplayName = "Ammo"),
+	EST_AmmoLeft UMETA(DisplayName = "Ammo Left"),
+	EST_Damage UMETA(DisplayName = "Damage"),
+	EST_Health UMETA(DisplayName = "Health"),
+	EST_Armor UMETA(DisplayName = "Armor"),
+	EST_Agility UMETA(DisplayName = "Agility"),
+	EST_FireRate UMETA(DisplayName = "Fire Rate"),
+	EST_Accuracy UMETA(DisplayName = "Accuracy")
+};
